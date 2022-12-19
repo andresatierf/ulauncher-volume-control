1b1af5ebb57c51664fc125473390d19e45768dbe
Andre Freitas <andrecfreitas58@gmail.com>
2022-12-19 14:20:56 +0000

    simplify

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+
+# from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 
 # logger = logging.getLogger(__name__)
@@ -16,7 +17,7 @@ class VolumeControlExtension(Extension):
     def __init__(self):
         super(VolumeControlExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+        # self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
 
 class KeywordQueryEventListener(EventListener):
@@ -29,9 +30,7 @@ class KeywordQueryEventListener(EventListener):
                     ExtensionResultItem(
                         icon="images/icon.png",
                         name=f"Set system volume to {query}",
-                        on_enter=ExtensionCustomAction(
-                            {"new_name": "Item s was clicked"}, keep_app_open=False
-                        ),
+                        on_enter=HideWindowAction(),
                     )
                 ]
             )
@@ -45,27 +44,25 @@ class KeywordQueryEventListener(EventListener):
                     icon="images/icon.png",
                     name="%s %s" % (item_name, i),
                     description="Item description %s" % i,
-                    on_enter=ExtensionCustomAction(
-                        {"new_name": "Item %s was clicked"}, keep_app_open=False
-                    ),
+                    on_enter=HideWindowAction(),
                 )
             )
 
         return RenderResultListAction(items)
 
 
-class ItemEnterEventListener(EventListener):
-    def on_event(self, event, extension):
-        data = event.get_data()
-        return RenderResultListAction(
-            [
-                ExtensionResultItem(
-                    icon="images/icon.png",
-                    name=data["new_name"],
-                    on_enter=HideWindowAction(),
-                )
-            ]
-        )
+# class ItemEnterEventListener(EventListener):
+#     def on_event(self, event, extension):
+#         data = event.get_data()
+#         return RenderResultListAction(
+#             [
+#                 ExtensionResultItem(
+#                     icon="images/icon.png",
+#                     name=data["new_name"],
+#                     on_enter=HideWindowAction(),
+#                 )
+#             ]
+#         )
 
 
 if __name__ == "__main__":
