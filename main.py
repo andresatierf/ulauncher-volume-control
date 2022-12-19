2c268997f57a7021d30e38d9443ee87dc8d0bf8c
Andre Freitas <andrecfreitas58@gmail.com>
2022-12-19 17:30:09 +0000

    working

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # import logging
 # from time import sleep
 import os
+import subprocess
+import re
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
@@ -9,8 +11,35 @@ from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.api.shared.action.SetUserQueryAction import SetUserQueryAction  # noqa
 
 # logger = logging.getLogger(__name__)
+
+
+def getPlayingApplications():
+    output = str(subprocess.check_output("pactl list sink-inputs", shell=True))
+    names = re.findall('application.name = "(.+?)"', output)
+    sinks = re.findall("Sink Input #(\d+)", output)
+    vols = re.findall("Volume: (.+?)\\\\n", output)
+
+    return [
+        {"name": name, "sink-input": sink, "vol": vol}
+        for name, sink, vol in zip(names, sinks, vols)
+    ]
+
+
+def createEntries(apps, action):
+    entries = []
+    for app in apps:
+        entries.append(
+            ExtensionResultItem(
+                icon="images/icon.png",
+                name=app["name"],
+                description=app["vol"],
+                on_enter=action(app),
+            )
+        )
+    return entries
 
 
 class VolumeControlExtension(Extension):
@@ -22,9 +51,18 @@ class VolumeControlExtension(Extension):
 
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
-        apps = [{"name": "spotify", "sink-input": "6"}]
-
+        apps = getPlayingApplications()
         query = event.get_argument()
+
+        if not query:
+            return RenderResultListAction(
+                createEntries(
+                    apps=apps,
+                    action=lambda app: SetUserQueryAction(
+                        f'{event.get_keyword()} {app["name"]}'
+                    ),
+                )
+            )
 
         if query and query.isnumeric():
             return RenderResultListAction(
@@ -44,28 +82,41 @@ class KeywordQueryEventListener(EventListener):
                 ]
             )
 
-        items = []
+        application, *volume = query.split()
         for app in apps:
-            items.append(
-                ExtensionResultItem(
-                    icon="images/icon.png",
-                    name=app["name"],
-                    on_enter=ExtensionCustomAction(
-                        {
-                            "cmd": "set-sink-input-volume",
-                            "sink": app["sink-input"],
-                            "vol": query.split()[1],
-                        }
+            print(application, app)
+            if application.lower() not in app["name"].lower():
+                apps.pop(app)
+
+        if not volume:
+            return RenderResultListAction(
+                createEntries(
+                    apps=apps,
+                    action=lambda app: SetUserQueryAction(
+                        f'{event.get_keyword()} {app["name"]}'
                     ),
                 )
             )
 
-        return RenderResultListAction(items)
+        return RenderResultListAction(
+            createEntries(
+                apps=apps,
+                action=lambda app: ExtensionCustomAction(
+                    {
+                        "cmd": "set-sink-input-volume",
+                        "sink": app["sink-input"],
+                        "vol": volume.pop(),
+                    },
+                    keep_app_open=False,
+                ),
+            )
+        )
 
 
 class ItemEnterEventListener(EventListener):
     def on_event(self, event, extension):
         data = event.get_data()
+        print(data)
         os.system(f"pactl {data['cmd']} {data['sink']} {data['vol']}%")
 
 
